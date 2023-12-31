f4709bfcc9c5e0a994c5723545a382edb688fc8b
haotvwork <haotvwork@gmail.com>
2023-12-31 12:56:20 +0700

    update env and tmp

--- vue-project/src/views/Task2View.vue
+++ vue-project/src/views/Task2View.vue
@@ -1,14 +1,35 @@
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 
 <template>
-    <div class="alert alert-success">
-        <h1>Dynamic FE Layout </h1>
+  <div class="alert alert-success">
+    <h1>Dynamic FE Layout </h1>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 bg-primary">
+        Item 1
+      </div>
     </div>
+    <div class="row">
+      <div class="col-md-4 col-sm-12 bg-success">
+        Item 2
+      </div>
+      <div class="col-md-8 col-sm-12">
+        <div class="row">
+          <div class="col-6 bg-secondary">Item 3</div>
+          <div class="col-6 bg-info">Item 4</div>
+        </div>
+        <div class="row">
+          <div class="col-12 bg-warning">
+            Item 5
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
